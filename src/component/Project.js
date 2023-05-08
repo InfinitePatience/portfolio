89ebb13d111172300941e91693ee9f4ce56ddebe
InfinitePatience <roysjk34@gmail.com>
2023-05-08 09:52:25 +0900

    commit

---
+++ src/component/Project.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/Project.scss';
+
+function Project() {
+  return (
+    <>
+    <div className='project_container'>
+      <div className='project_animation'></div>
+      <div className='project_moveproject'></div>
+      <div className='project_title'>
+        <span>
+          <p>#web / #accessibility</p>
+          <p>DAVICH GLASSES</p>
+          <p>RESPONSIVE WEB</p>
+        </span>
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default Project
